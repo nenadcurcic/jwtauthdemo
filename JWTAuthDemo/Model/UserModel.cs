ac5f08f4c9d2fe0d3286483af2e26e76879bc537
shadowue@hotmail.com <shadowue@hotmail.com>
2020-08-22 11:44:07 +0200

    Update UserModel

--- JWTAuthDemo/Model/UserModel.cs
+++ JWTAuthDemo/Model/UserModel.cs
@@ -7,5 +7,6 @@
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Role { get; set; }
     }
 }
